b5219336f3541bcbf11aff62f7824255cc31ef3a
seanmct <seanmct@vt.edu>
2020-02-11 12:21:42 -0500

    run init.sh and better naming

--- wpa_init.py
+++ wpa_init.py
@@ -1,25 +1,26 @@
 import subprocess
 
+subprocess.run(["touch", "/etc/wpa_supplicant/wpa_supplicant.conf"])
 subprocess.run(["cp", "/etc/wpa_supplicant/wpa_supplicant.conf", "/etc/wpa_supplicant/wpa_supplicant.conf"])
 subprocess.run(["cp", "/etc/network/interfaces", "/etc/network/interfaces"])
 
-f= open("/etc/wpa_supplicant/wpa_supplicant.conf","a")
+with open("/etc/wpa_supplicant/wpa_supplicant.conf","a") as f:
 
 
 ##need to add a check for ssid="eduroam" so that this can berun a million times without issue
 ##this will be changed to python3 in the ideal future
 ##python 3 will handle the password input with stdiomask
-f.write("network={")
-f.write("        ssid=\"eduroam\"")
-f.write("        scan_ssid=1")
-f.write("        key_mgmt=WPA-EAP")
-f.write("        eap=PEAP")
-f.write("        identity=/"<pid>@vt.edu/"")
-f.write("        password=/"<network password>/"")
-f.write("        phase1=/"peaplabel=0/"")
-f.write("        phase2=/"auth=MSCHAPV2/"")
-f.write("}")
-f.close()
+    f.write("network={")
+    f.write("        ssid=\"eduroam\"")
+    f.write("        scan_ssid=1")
+    f.write("        key_mgmt=WPA-EAP")
+    f.write("        eap=PEAP")
+    #f.write("        identity=/"<pid>@vt.edu/"")
+    f.write("        password=/"<network password>/"")
+    f.write("        phase1=/"peaplabel=0/"")
+    f.write("        phase2=/"auth=MSCHAPV2/"")
+    f.write("}")
+
 
 i= open("/etc/network/interfaces","a")
 
